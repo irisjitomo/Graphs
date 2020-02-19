bb474cfb32b7f06ceb62b05dd212a04124bd854f
unknown <irisjitomo@yahoo.com>
2020-02-18 19:29:20 -0500

    atempted ancestor'

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,22 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+from graph import Graph
+from util import Stack, Queue  # These may come in handy
+
+
+def earliest_ancestor(ancestors, starting_node, visited=None):
+    parents = []
+    for ancestor in ancestors:
+        if ancestor[1] == starting_node:
+            print(ancestor[0])
+            parents.append(ancestor[0])
+            starting_node = ancestor[0]
+    if len(parents) < 1:
+        return -1
+    elif len(parents) == 2:
+        return parents[-1]
+        #return parents
+    elif len(parents) > 2:
+        return min(parents)
+    else:
+        return parents[0]
+        # return parents
--- projects/ancestor/graph.py
+++ projects/ancestor/graph.py
@@ -3,7 +3,7 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
-class Graph:
+class Graph():
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -13,14 +13,14 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        self.vertices(vertex) = set()
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices(v1).add(v2)
+            self.vertices[v1].add(v2)
         else:
             raise IndexError("Can't create edge based on given vertices")
 
@@ -111,7 +111,7 @@ class Graph:
             v = path[-1]
             # CHECK IF IT'S THE TARGET
             # Check if it's been visited
-            if v === destination_vertex:
+            if v == destination_vertex:
                 # IF SO, RETURN THE PATH
                 return path
             # If it has not been visited...
@@ -137,7 +137,7 @@ class Graph:
         while s.size() > 0:
             path = s.pop()
             v = path[-1]
-            if v === destination_vertex:
+            if v == destination_vertex:
                 return v
             if v not in visited:
                 visited.add(v)
@@ -178,71 +178,3 @@ class Graph:
                 new_path = self.dfs_recursive(neighbor, destination_vertex, visited, path_copy)
                 if new_path is not None:
                     return new_path
-
-if __name__ == '__main__':
-    graph = Graph()  # Instantiate your graph
-    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
-    graph.add_vertex(1)
-    graph.add_vertex(2)
-    graph.add_vertex(3)
-    graph.add_vertex(4)
-    graph.add_vertex(5)
-    graph.add_vertex(6)
-    graph.add_vertex(7)
-    graph.add_edge(5, 3)
-    graph.add_edge(6, 3)
-    graph.add_edge(7, 1)
-    graph.add_edge(4, 7)
-    graph.add_edge(1, 2)
-    graph.add_edge(7, 6)
-    graph.add_edge(2, 4)
-    graph.add_edge(3, 5)
-    graph.add_edge(2, 3)
-    graph.add_edge(4, 6)
-
-    '''
-    Should print:
-        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
-    '''
-    print(graph.vertices)
-
-    '''
-    Valid BFT paths:
-        1, 2, 3, 4, 5, 6, 7
-        1, 2, 3, 4, 5, 7, 6
-        1, 2, 3, 4, 6, 7, 5
-        1, 2, 3, 4, 6, 5, 7
-        1, 2, 3, 4, 7, 6, 5
-        1, 2, 3, 4, 7, 5, 6
-        1, 2, 4, 3, 5, 6, 7
-        1, 2, 4, 3, 5, 7, 6
-        1, 2, 4, 3, 6, 7, 5
-        1, 2, 4, 3, 6, 5, 7
-        1, 2, 4, 3, 7, 6, 5
-        1, 2, 4, 3, 7, 5, 6
-    '''
-    graph.bft(1)
-
-    '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    graph.dft(1)
-    graph.dft_recursive(1)
-
-    '''
-    Valid BFS path:
-        [1, 2, 4, 6]
-    '''
-    print(graph.bfs(1, 6))
-
-    '''
-    Valid DFS paths:
-        [1, 2, 4, 6]
-        [1, 2, 4, 7, 6]
-    '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
